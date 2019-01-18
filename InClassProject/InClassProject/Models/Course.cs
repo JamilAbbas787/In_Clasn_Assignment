4ed42b081c697a0c807eef1118723da886f37d11
Jamil Abbas <jamilabbas787@gmail.com>
2019-01-17 21:14:07 -0500

    first

---
+++ InClassProject/InClassProject/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace InClassProject.Models
+{
+    public class Course
+    {
+    
+        public int CourseID { get; set; }
+
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        
+    }
+}
